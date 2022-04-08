f6676ba2c344a6fa3e4be9a2a61b4814485a4410
Melih Kurtaran <melihkurtaran@sabanciuniv.edu>
2022-04-08 09:11:45 +0300

    Update publish.js

--- util/publish.js
+++ util/publish.js
@@ -1,1 +1,1 @@
-require('@vaadin-component-factory/vcf-auto-select-combobox-util').publish('vcf-auto-select-combobox', __dirname);
+require('@vaadin-component-factory/vcf-auto-select-combobox').publish('vcf-auto-select-combobox', __dirname);
