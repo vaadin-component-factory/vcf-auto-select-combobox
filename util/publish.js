12725dcc7cf5611c3aca6bcc4c33c2f88707fc16
jcgueriaud1 <jean-christophe@vaadin.com>
2024-03-12 13:23:28 +0200

    v23.4.0

--- util/publish.js
+++ util/publish.js
@@ -1,1 +1,8 @@
-publish('vcf-auto-select-combobox', __dirname);
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { publish } from '@vaadin-component-factory/vcf-element-util';
+
+const __filename = fileURLToPath(import.meta.url);
+
+const __dirname = path.dirname(__filename);
+publish('vcf-auto-select-combobox', __dirname, 'src', 'js');
